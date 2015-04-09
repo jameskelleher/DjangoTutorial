ca4d7f6c05908581305f62034ff551c7735c1343
James Kelleher <jk3047@nyu.edu>
2015-04-08 23:07:28 -0400

    continuing with part 2

--- polls/models.py
+++ polls/models.py
@@ -9,9 +9,13 @@ class Question(models.Model):
         
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published Recently?'
     
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    search_fields = ['question_text']
     
 class Choice(models.Model):
     def __str__(self):
